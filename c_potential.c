65676c119d118ce43cd060f7a0755837478789d9
Alex Gurvich <tg835099@login3.stampede2.tacc.utexas.edu>
2018-11-19 17:07:48 -0600

    added sub sampling pairwise distances

--- c_potential.c
+++ c_potential.c
@@ -63,11 +63,43 @@ int calcDists(
     return 1;
 }
 
+int calcTestPairwiseDistsSquared(
+    int Ntest,
+    float * test_xs, float * test_ys, float * test_zs,
+    int Narr,
+    float * xs, float * ys, float * zs,
+    int count_skip,
+    float * H_OUT ){
+
+    float x,y,z;
+    float dx,dy,dz;
+    int count = 0;
+    // loop over each point in the array
+    for (int i=0; i<Ntest; i++){
+        // pick out this point
+        // if test_xs is xs[:Ntest] then you can iteratively call this function 
+        //  externally to save memory
+        x = test_xs[i];
+        y = test_ys[i];
+        z = test_zs[i];
+        // loop over the remaining points, starting with the next one
+        // NOTE the fact that j = i+1 to start **assumes** that test_xs = xs[:Ntest]
+        for (int j=i+1; j<Narr; j++){
+            // calculate the separation
+            dx = x - xs[j];
+            dy = y - ys[j];
+            dz = z - zs[j];
+            // put this square distance into the output array and move onto the next
+            H_OUT[count+count_skip]=dx*dx + dy*dy + dz*dz;
+            count++;
+        }
+    }
+}
+
 int calcPairwiseDistsSquared(
     int Narr,
     float * xs, float * ys, float * zs,
     float * H_OUT ){
-
     float x,y,z;
     float dx,dy,dz;
     int count = 0;
@@ -90,3 +122,4 @@ int calcPairwiseDistsSquared(
         }
     }
 }
+
--- potential_utils.py
+++ potential_utils.py
@@ -3,40 +3,135 @@ import os
 import numpy as np 
 import ctypes
 import copy
+import time
+from memory_profiler import profile,memory_usage
 
+def enter(sleep_time=1):
+    init_time = time.time()
+    while time.time() - init_time < sleep_time:
+        pass
+    pass
 
-def calculateCPairwiseDistancesSquared(all_pos):
-    ## recast and copy to safe memory
-    all_pos = all_pos.astype('f')
-    xs,ys,zs = all_pos.T
-    xs = copy.copy(xs)
-    ys = copy.copy(ys)
-    zs = copy.copy(zs)
+def exit(sleep_time=1):
+    init_time = time.time()
+    while time.time() - init_time < sleep_time:
+        pass
+    pass
+    
+def subCalculateCPairwiseDistancesSquared(
+    ncutoff,
+    run,nruns,
+    xs,ys,zs,
+    fin_array,
+    count,tot_count):
 
-    Narr = all_pos.shape[0]
+    enter()
+    print("Working on subset",run+1,"out of",nruns)
+    test_xs = xs[run*ncutoff:(run+1)*ncutoff]
+    test_ys = ys[run*ncutoff:(run+1)*ncutoff]
+    test_zs = zs[run*ncutoff:(run+1)*ncutoff]
+    Ntest = test_xs.shape[0]
+    Narr = xs.shape[0]
 
-    ## prepare the output pointer
-    ##  sum of arithmetic series = n*(n+1)/2 with n = Narr -1
-    h_out_cast=ctypes.c_float*( (Narr-1)*(Narr+1-1)/2 )
-    H_OUT=h_out_cast()
+    #for i in range(Ntest):
+        #arr_size += (Narr-1)-count-i
+
+    ## partial arithmetic series sum formula 
+    ##  n/2 (a1 + an) 
+    arr_size = int(Ntest/2.0 * (Narr - 1 - count + Narr - count - Ntest))
+
+    new_xs = xs[count:]
+    new_ys = ys[count:]
+    new_zs = zs[count:]
+
+    print(Ntest,'this run -> ',arr_size,'many distances')
 
     ## call the c executable
     exec_call = os.path.join(os.environ['HOME'],"python/CPotential/c_potential.so")
     c_obj = ctypes.CDLL(exec_call)
 
     print("Calling the C executable...",)
-    c_obj.calcPairwiseDistsSquared(
-        ctypes.c_int(Narr),
-
-        xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
-        ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
-        zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
-
-        H_OUT)
+    c_obj.calcTestPairwiseDistsSquared(
+        ctypes.c_int(Ntest),
+        test_xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+        test_ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+        test_zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+        ctypes.c_int(Narr-count),
+        new_xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+        new_ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+        new_zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+        ctypes.c_int(tot_count),
+        fin_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float)))
     print("... done!",)
 
-    h=np.ctypeslib.as_array(H_OUT)
-    return h
+    exit()
+    return Ntest,arr_size
+
+def calculateCPairwiseDistancesSquared(all_pos,ncutoff=2e4):
+    ncutoff = int(ncutoff)
+
+    ## recast and copy to safe memory
+    all_pos = all_pos.astype('f')
+    xs,ys,zs = all_pos.T
+
+    Narr = all_pos.shape[0]
+
+    ## arithmetic series sum formula
+    ##  sum of arithmetic series = n*(n+1)/2 with n = Narr -1
+    big_dist_count = (Narr-1)*(Narr+1-1)/2
+
+    if Narr > ncutoff:
+        nruns = int(Narr/ncutoff) + ((Narr%ncutoff)!=0)
+
+        #big_dist_count
+        fin_array = np.zeros(10**10,dtype=np.float32)
+
+        ## accumulation variables
+        count = 0
+        tot_count = 0
+
+        for run in xrange(nruns):
+            Ntest,arr_size = subCalculateCPairwiseDistancesSquared(
+            ncutoff,
+            run,nruns,
+            xs,ys,zs,
+            fin_array,
+            count,tot_count)
+
+            count += Ntest
+            tot_count += arr_size
+
+        ## make sure we calculated a distance to all the points
+        assert tot_count==big_dist_count
+
+        return fin_array
+    else:
+        xs = copy.copy(xs)
+        ys = copy.copy(ys)
+        zs = copy.copy(zs)
+
+        ## prepare the output pointer
+        ##  sum of arithmetic series = n*(n+1)/2 with n = Narr -1
+        h_out_cast=ctypes.c_float*(big_dist_count)
+        H_OUT=h_out_cast()
+
+        ## call the c executable
+        exec_call = os.path.join(os.environ['HOME'],"python/CPotential/c_potential.so")
+        c_obj = ctypes.CDLL(exec_call)
+
+        print("Calling the C executable...",)
+        c_obj.calcPairwiseDistsSquared(
+            ctypes.c_int(Narr),
+            xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+
+            H_OUT)
+        print("... done!",)
+
+        h=np.ctypeslib.as_array(H_OUT)
+        print(h.shape[0],'many distances')
+        return h
 
 def calculateCPotential(all_pos,all_masses,test_pos):
     ## recast 
@@ -117,6 +212,7 @@ def test_pair(all_pos):
 
 if __name__ == '__main__':
 
+    """
     dec_pos = np.array([[0,0,0]]*5)
     dec_mass = np.array([1,1,1,1,1])
     test_pos = np.array([[0,0,1]]*3)
@@ -125,4 +221,11 @@ if __name__ == '__main__':
 
     all_pos = np.array([[1,1,1],[2,2,2],[1,1,1],[2,2,2]])
     test_pair(all_pos)
+    """
+
+
+    coords = np.random.random(size=(int(7e4),3))
+    init_time = time.time()
+    calculateCPairwiseDistancesSquared(coords,ncutoff=1e4)
+    print(time.time()-init_time,'s elapsed calculating pairwise distances')
 
