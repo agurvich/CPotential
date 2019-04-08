00fbdaefc3d4ff48c605f0efb3045445ffa56aff
Alex Gurvich <agurvich@u.northwestern.edu>
2019-04-08 17:04:41 -0500

    implement relatively efficient multi-threaded solution (~x2 speedup is lost for some reason... approaches nthreads x speed-up when ntest*narr > 1e9 or 1e10)

--- potential_utils.py
+++ potential_utils.py
@@ -5,6 +5,10 @@ import ctypes
 import copy
 import time
 from memory_profiler import profile,memory_usage
+import itertools
+import warnings
+
+import pdb
 
 def enter(sleep_time=1):
     init_time = time.time()
@@ -46,7 +50,7 @@ def subCalculateCPairwiseDistancesSquared(
     print(Ntest,'this run -> ',arr_size,'many distances')
 
     ## call the c executable
-    exec_call = os.path.join(os.environ['HOME'],"python/CPotential/c_potential.so")
+    exec_call = os.path.join(os.environ['HOME'],"python/CPotential/src/c_potential.so")
     c_obj = ctypes.CDLL(exec_call)
 
     print("Calling the C executable...",)
@@ -76,7 +80,7 @@ def calculateCPairwiseDistancesSquared(all_pos,ncutoff=2e4):
 
     ## arithmetic series sum formula
     ##  sum of arithmetic series = n*(n+1)/2 with n = Narr -1
-    big_dist_count = (Narr-1)*(Narr+1-1)/2
+    big_dist_count = (Narr-1)*(Narr+1-1)//2
 
     if Narr > ncutoff:
         nruns = int(Narr/ncutoff) + ((Narr%ncutoff)!=0)
@@ -87,7 +91,7 @@ def calculateCPairwiseDistancesSquared(all_pos,ncutoff=2e4):
         count = 0
         tot_count = 0
 
-        for run in xrange(nruns):
+        for run in range(nruns):
 
             test_xs = xs[run*ncutoff:(run+1)*ncutoff]
             test_ys = ys[run*ncutoff:(run+1)*ncutoff]
@@ -120,7 +124,7 @@ def calculateCPairwiseDistancesSquared(all_pos,ncutoff=2e4):
         H_OUT=h_out_cast()
 
         ## call the c executable
-        exec_call = os.path.join(os.environ['HOME'],"python/CPotential/c_potential.so")
+        exec_call = os.path.join(os.environ['HOME'],"python/CPotential/src/c_potential.so")
         c_obj = ctypes.CDLL(exec_call)
 
         print("Calling the C executable...",)
@@ -137,7 +141,7 @@ def calculateCPairwiseDistancesSquared(all_pos,ncutoff=2e4):
         print(h.shape[0],'many distances')
         return h
 
-def calculateCPotential(all_pos,all_masses,test_pos):
+def calculateCPotential(all_pos,all_masses,test_pos,print_flag = 1,nthreads =1):
     ## recast 
     all_pos = all_pos.astype('f')
     all_masses = all_masses.astype('f')
@@ -147,15 +151,15 @@ def calculateCPotential(all_pos,all_masses,test_pos):
     ## unpack positions
     xs,ys,zs = all_pos.T
      ## need to reallocate arrays in memory 
-    xs = copy.copy(xs)
-    ys = copy.copy(ys)
-    zs = copy.copy(zs)
+    xs = np.ascontiguousarray(xs)#copy.copy(xs)
+    ys = np.ascontiguousarray(ys)#copy.copy(ys)
+    zs = np.ascontiguousarray(zs)#copy.copy(zs)
 
     test_xs,test_ys,test_zs = test_pos.T
     ## need to reallocate arrays in memory 
-    test_xs = copy.copy(test_xs)
-    test_ys = copy.copy(test_ys)
-    test_zs = copy.copy(test_zs)
+    test_xs = np.ascontiguousarray(test_xs)#copy.copy(test_xs)
+    test_ys = np.ascontiguousarray(test_ys)#copy.copy(test_ys)
+    test_zs = np.ascontiguousarray(test_zs)#copy.copy(test_zs)
 
     Narr = all_pos.shape[0]
     Ntest = test_pos.shape[0]
@@ -167,31 +171,60 @@ def calculateCPotential(all_pos,all_masses,test_pos):
     ## call the c executable
 
     ## alternatively can link to this __file__'s location
-    exec_call = os.path.join(os.environ['HOME'],"python/CPotential/c_potential.so")
+    exec_call = os.path.join(os.environ['HOME'],"python/CPotential/src/c_potential.so")
     c_obj = ctypes.CDLL(exec_call)
 
-    print("Calling the C executable...",)
-    c_obj.calcDists(
-        ctypes.c_int(Narr),
-        xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
-        ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
-        zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
 
-        all_masses.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), #masses
+    ## from "extensive" testing this seems to be around the point where you get
+    ##  consistent speedup. interestingly increasing either Ntest or Narr will
+    ##  do it (even though only the Narr portion is actually multi-threaded). 
+    if np.log10(Narr*Ntest) < 8:
+        warnings.warning("Not enough points for multi-threading to be useful, setting threads to 1")
+        nthreads = 1
 
-        ctypes.c_int(Ntest),
-        test_xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
-        test_ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
-        test_zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+    if nthreads >1:
+        if print_flag:
+            print("Calling the Multi-C executable...",)
+        c_obj.multiCalculateZGravityAtLocations(
+            ctypes.c_int(nthreads),
+            ctypes.c_int(Narr),
+            xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
 
-        ctypes.byref(H_OUT))
-    print("...done!")
+            all_masses.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), #masses
+
+            ctypes.c_int(Ntest),
+            test_xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            test_ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            test_zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+
+            ctypes.byref(H_OUT))
+    else:
+        if print_flag:
+            print("Calling the C executable...",)
+        c_obj.calculateZGravityAtLocations(
+            ctypes.c_int(Narr),
+            xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+
+            all_masses.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), #masses
+
+            ctypes.c_int(Ntest),
+            test_xs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            test_ys.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            test_zs.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+
+            ctypes.byref(H_OUT))
+    if print_flag:
+        print("...done!")
 
     h=np.ctypeslib.as_array(H_OUT)
     return h
 
 
-def test_pot(all_pos,all_masses,test_pos):
+def test_pot(all_pos,all_masses,test_pos,nthreads =1):
     ## py-calculate
     from scipy.spatial.distance import cdist as cdist
     distss = cdist(test_pos,all_pos)
@@ -203,11 +236,11 @@ def test_pot(all_pos,all_masses,test_pos):
     py_grav_cgs = np.sum(all_masses/distss**2*zcomponents,axis=1)*ALLTOGETHER
 
     ## c-calculate
-    pos_cgs_zgravs = calculateCPotential(all_pos,all_masses,test_pos)
+    pos_cgs_zgravs = calculateCPotential(all_pos,all_masses,test_pos,print_flag = True,nthreads=nthreads)
 
     rats = py_grav_cgs / pos_cgs_zgravs
 
-    print(rats)
+    print('Test passed:',np.all((rats-1)**2 < 1e8))
 
     return rats
 
@@ -216,20 +249,32 @@ def test_pair(all_pos):
 
 if __name__ == '__main__':
 
-    """
-    dec_pos = np.array([[0,0,0]]*5)
-    dec_mass = np.array([1,1,1,1,1])
-    test_pos = np.array([[0,0,1]]*3)
-    #print(test_pos)
-    #test_pot(dec_pos,dec_mass,test_pos)
 
+    nmasses = int(5e5)
+    ntest = 50*4
+    nthreads = 10
+    dec_pos = np.random.random(size=(nmasses,3))
+    dec_mass = np.ones(nmasses)
+    test_pos = np.random.random(size=(ntest,3))
+    init_time = time.time()
+    print('%.1e elements'%nmasses)
+    #test_pot(dec_pos,dec_mass,test_pos,nthreads = nthreads)
+    calculateCPotential(dec_pos,dec_mass,test_pos,print_flag = True,nthreads=nthreads)
+    multi_dur = time.time()-init_time
+    calculateCPotential(dec_pos,dec_mass,test_pos,print_flag = True,nthreads=1)
+    #test_pot(dec_pos,dec_mass,test_pos,nthreads = 1)
+    single_dur = time.time()-init_time - multi_dur
+    print('1 thread: %.4f s'%single_dur,'%d threads: %.4f s'%(nthreads,multi_dur))
+    print(single_dur/multi_dur,'X speedup')
+
+    """
     all_pos = np.array([[1,1,1],[2,2,2],[1,1,1],[2,2,2]])
     test_pair(all_pos)
-    """
 
+    coords = np.random.random(size=(int(5e4),3))
 
-    coords = np.random.random(size=(int(7e4),3))
-    init_time = time.time()
     calculateCPairwiseDistancesSquared(coords,ncutoff=1e4)
     print(time.time()-init_time,'s elapsed calculating pairwise distances')
+    """
+    
 
--- src/multi_potential.c
+++ src/multi_potential.c
@@ -14,16 +14,17 @@
 
 float multiCalculateGravityZ(float * point, float * masses, float * xs, float * ys, float * zs,int Narr,int nthreads){
     float total_grav = 0;
-    #pragma omp parallel for num_threads(nthreads) shared(sum, total_grav) reduction(+: sum)
-    for (int i =0; i<Narr; i++){
+    int i = 0;
+    #pragma omp parallel for num_threads(nthreads) shared(xs,ys,zs,point,masses) reduction(+:total_grav) private(i)
+    for (i=0;i<Narr; i++){
         float dx = xs[i]-point[0];
         float dy = ys[i]-point[1];
         float dz = zs[i]-point[2];
 
         float dr = sqrt(dx*dx + dy*dy + dz*dz); // RIP speed
-        summ += masses[i]/(dr*dr*dr)*dz; 
+        total_grav += masses[i]/(dr*dr*dr)*dz; 
     }
-    return ALLTOGETHER*summ;//cgs
+    return ALLTOGETHER*total_grav;//cgs
 }
 
 int multiCalculateZGravityAtLocations(
@@ -36,11 +37,13 @@ int multiCalculateZGravityAtLocations(
     float * H_OUT ){
 
     float point[3];
+    int i = 0;
+    //#pragma omp parallel private(i) num_threads(2)
     for (int i=0; i<Ntest; i++){
 
         point[0]=test_xs[i]; point[1]=test_ys[i]; point[2]=test_zs[i];
 
-        H_OUT[i]=calculateGravityZ(point,masses,xs,ys,zs,Narr);
+        H_OUT[i]=multiCalculateGravityZ(point,masses,xs,ys,zs,Narr,nthreads);
 
     }
 
